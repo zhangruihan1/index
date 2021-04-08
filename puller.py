181ca892b9472612e30719eaad342d01e4aa3085
cestwc <80936226+cestwc@users.noreply.github.com>
2021-04-08 16:52:30 +0800

    Update puller.py

--- puller.py
+++ puller.py
@@ -3,12 +3,11 @@ import csv
 import json
 import gdown
 
-with open('index.csv', 'r') as f:
-	reader = csv.reader(f)
-	
 urls = {}
-for row in reader:
-	urls[row[0]] = row[1].replace("file/d/", "uc?id=").replace("/view?usp=sharing", '')
+with open('index.csv', 'r') as f:
+	reader = csv.reader(f)	
+	for row in reader:
+		urls[row[0]] = row[1].replace("file/d/", "uc?id=").replace("/view?usp=sharing", '')
 
 def pull(name):
 	if not os.path.exists(name):
